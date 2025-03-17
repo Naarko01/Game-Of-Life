a0de29bd63d23ee164bf1bf0ac5370ccb00739ee
Maxime <maxime.drouhin71@gmail.com>
2025-03-17 13:01:16 +0100

    fonctionnal with maps instead of arrays

--- script.js
+++ script.js
@@ -9,13 +9,12 @@ const CELL_SIZE = 10;
 const GRID_WIDTH = canvas1.width / CELL_SIZE;
 const GRID_HEIGHT = canvas1.height / CELL_SIZE;
 
-let grid = createEmptyGrid();
-let nextGrid = createEmptyGrid();
 let isRunning = false;
 let timer;
-let generationTime = 25;
+let generationTime = 40;
 let linesToggled = false;
-let aliveCells = createEmptyMap(); //initialize aliveCells map
+let aliveCells = new Map(); //initialize aliveCells map
+let nextAliveCells = new Map(); //initialize next generation aliveCells map
 let cellsToCheck = createCellsToCheckMap(); //initialize cellsToCheck map
 
 let isDragging = false;
@@ -77,41 +76,22 @@ canvas1.addEventListener('click', (event) => {
     const rect = canvas1.getBoundingClientRect();
     const scaleX = canvas1.width / rect.width;
     const scaleY = canvas1.height / rect.height;
-
     const x = Math.floor((event.clientX - rect.left) * scaleX / CELL_SIZE);
     const y = Math.floor((event.clientY - rect.top) * scaleY / CELL_SIZE);
     console.log(`Clicked on grid cell[${x}, ${y}]`);
-
     if (x >= 0 && x < GRID_WIDTH && y >= 0 && y < GRID_HEIGHT) {
         //swap cell state
-        aliveCells.set(`${x},${y}`, 1);
+        if (aliveCells.has(`${x},${y}`)) {
+            aliveCells.delete(`${x},${y}`);
+        } else {
+            aliveCells.set(`${x},${y}`, 1);
+        }
         drawGrid();
     }
 });
 
-function createEmptyMap() {
-    const map = new Map();
-    for (let x = 0; x < GRID_WIDTH; x++) {
-        for (let y = 0; y < GRID_HEIGHT; y++) {
-            map.set(`${x},${y}`, 0);
-        }
-    }
-    return map;
-}
-
-function createEmptyGrid() {
-    const grid = new Array(GRID_WIDTH);
-
-    for (let x = 0; x < GRID_WIDTH; x++) {
-        grid[x] = new Array(GRID_HEIGHT).fill(0);
-    }
-
-    return grid;
-}
-
 function drawGrid() {
     ctx1.clearRect(0, 0, canvas1.width, canvas1.height);
-
     // draw allive cells
     ctx1.fillStyle = 'white';
     for (let x = 0; x < GRID_WIDTH; x++) {
@@ -121,30 +101,25 @@ function drawGrid() {
             }
         }
     }
-
     //draw grid lines if toggled
     if (linesToggled) {
         drawGridLines();
     }
-
 }
 
 function drawGridLines() {
     ctx1.strokeStyle = 'white';
     ctx1.lineWidth = 0.5;
     ctx1.beginPath();
-
     for (let x = 0; x <= GRID_WIDTH; x++) {
         ctx1.moveTo(x * CELL_SIZE, 0);
         ctx1.lineTo(x * CELL_SIZE, canvas1.height);
     }
-
     for (let y = 0; y <= GRID_HEIGHT; y++) {
 
         ctx1.moveTo(0, y * CELL_SIZE);
         ctx1.lineTo(canvas1.width, y * CELL_SIZE);
     }
-
     ctx1.stroke();
 }
 
@@ -152,20 +127,21 @@ function toggleLines() {
     console.log('lines toggled');
     linesToggled = !linesToggled;
     drawGrid();
-
 }
 
 function clearGrid() {
     aliveCells.clear();
     cellsToCheck.clear();
+    nextAliveCells.clear();
     drawGrid();
 }
 
 function randomizeGrid() {
     for (let x = 0; x < GRID_WIDTH; x++) {
         for (let y = 0; y < GRID_HEIGHT; y++) {
-            //grid[x][y] = Math.random() > 0.9 ? 1 : 0;
-            aliveCells.set(`${x},${y}`, Math.random() > 0.9 ? 1 : 0);
+            if (Math.random() > 0.9) {
+                aliveCells.set(`${x},${y}`, 1);
+            }
         }
     }
     drawGrid();
@@ -175,7 +151,6 @@ function randomizeGrid() {
 function toggleSim() {
     isRunning = !isRunning;
     startButton.textContent = isRunning ? 'Stop' : 'Start';
-
     if (isRunning) {
         runSim();
     }
@@ -185,61 +160,52 @@ function toggleSim() {
 }
 
 function runSim() {
+    cellsToCheck = createCellsToCheckMap();
     nextGen();
     drawGrid();
-
     if (isRunning) {
         timer = setTimeout(runSim, generationTime);
     }
+    console.log(cellsToCheck.size);
 }
 
 function nextGen() {
-
-    nextGrid = createEmptyGrid();
-
-    for (let x = 0; x < GRID_WIDTH; x++) {
-        for (let y = 0; y < GRID_HEIGHT; y++) {
-            const neighbors = countAliveNeighbors(x, y);
-            //game of life rules
-            //if targeted cell is alive
-            if (aliveCells.has(`${x},${y}`)) {
-                //an alive cell with 2 or 3 alive neighbors stay alive
-                if (neighbors === 2 || neighbors === 3) {
-                    nextGrid[x][y] = 1;
-                }
-            } else {
-                //a dead cell with 3 alive neighbors becomes alive
-                if (neighbors === 3) {
-                    nextGrid[x][y] = 1;
-                }
+    for (const [key] of cellsToCheck.entries()) {
+        const [x, y] = key.split(',').map(Number);
+        const neighbors = countAliveNeighbors(x, y);
+        //game of life rules
+        //if targeted cell is alive
+        if (aliveCells.has(`${x},${y}`)) {
+            //an alive cell with 2 or 3 alive neighbors stay alive
+            if (neighbors === 2 || neighbors === 3) {
+                nextAliveCells.set(`${x},${y}`, 1);
             }
-
+        } else {
+            //a dead cell with 3 alive neighbors becomes alive
+            if (neighbors === 3) {
+                nextAliveCells.set(`${x},${y}`, 1);
+            }
         }
     }
     //swap grids
-    const temp = grid;
-    grid = nextGrid;
-    nextGrid = temp;
+    [aliveCells, nextAliveCells] = [nextAliveCells, aliveCells];
+    nextAliveCells.clear();
 
 }
 
 function countAliveNeighbors(x, y) {
     let count = 0;
-
     //check 8 neighbors of the given cell
     for (let i = -1; i <= 1; i++) {
         for (let j = -1; j <= 1; j++) {
             //don't chech the targeted cell
             if (i === 0 && j === 0) continue;
-
             const neighborX = (x + i + GRID_WIDTH) % GRID_WIDTH; //give the x coordinate of the neighbor to check
             const neighborY = (y + j + GRID_HEIGHT) % GRID_HEIGHT; //give the y coordinate of the neighbor to check
-
             //increment count by 1 if the cell is in the aliveCells map or by 0 if not
             count += aliveCells.get(`${neighborX},${neighborY}`) || 0;
         }
     }
-
     return count;
 }
 
@@ -263,33 +229,15 @@ function createCellsToCheckMap() {
                 const neighborY = (y + j + GRID_HEIGHT) % GRID_HEIGHT; //give the y coordinate of the neighbor to check
                 const neighborKey = `${neighborX},${neighborY}`; //give the key of the neighbor cell in the map
 
-                if (aliveCells.has(neighborKey)) {
-                    cellsToCheck.set(neighborKey, 1);
-                }
-                else {
+                if (!aliveCells.has(neighborKey)) {
                     cellsToCheck.set(neighborKey, 0);
                 }
 
             }
         }
     }
-
     return cellsToCheck;
 }
-
-/*function createAliveCellsMap(){
-    const aliveCells = new Map();
-
-    for (let x = 0; x < GRID_WIDTH; x++) {
-        for (let y = 0; y < GRID_HEIGHT; y++) {
-            if (grid[x][y] === 1) {
-                aliveCells.set(`${x},${y}`, 1);
-            }
-        }
-    }
-    
-    return aliveCells;
-}*/
 
 window.onload = () => {
     drawGrid();
